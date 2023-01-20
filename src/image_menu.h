319a797f5545edff9687b529a103ada22a6d2764
Anjoliekate <anjoliekatecummins@gmail.com>
2023-01-20 16:59:19 -0700

    changed image_menu

--- src/image_menu.h
+++ src/image_menu.h
@@ -1,8 +1,11 @@
 #include <string>
 #include <iostream>
+#ifndef image_menu.h
 
 std::string getString( std::istream& is, std::ostream& os, const std::string& prompt);
 int getInteger( std::istream& is, std::ostream& os, const std::string& prompt);
 double getDouble( std::istream& is, std::ostream& os, const std::string& prompt);
 int askQuestions3(std::istream& is, std::ostream& os);
 int assignment1( std::istream& is, std::ostream& os);
+
+#endif
