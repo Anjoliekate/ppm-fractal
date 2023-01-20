c362034c4733566443e3a4f3cb30a4a66ae95bc8
Anjoliekate <anjoliekatecummins@gmail.com>
2023-01-19 22:07:55 -0700

    fixes to image MENU

--- src/image_menu.h
+++ src/image_menu.h
@@ -1,8 +1,8 @@
 #include <string>
 #include <iostream>
 
-std::string getString( std::istream& is, std::ostream& os, const std::string& prompt );
-int getInteger( std::istream& is, std::ostream& os, const std::string& prompt );
-double getDouble( std::istream& is, std::ostream& os, const std::string& prompt );
+std::string getString( std::istream& is, std::ostream& os, const std::string& prompt);
+int getInteger( std::istream& is, std::ostream& os, const std::string& prompt);
+double getDouble( std::istream& is, std::ostream& os, const std::string& prompt);
 int askQuestions3(std::istream& is, std::ostream& os);
-int assignment1( std::istream& is, std::ostream& os );
+int assignment1( std::istream& is, std::ostream& os);
