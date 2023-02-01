201dc7bc27bd93e3a88105e665e19f3ae540b5b2
Anjoliekate <anjoliekatecummins@gmail.com>
2023-01-31 20:38:33 -0700

    ascii image working

--- src/controllers.cpp
+++ src/controllers.cpp
@@ -5,7 +5,7 @@
 int assignment1( std::istream& is, std::ostream& os){
     return askQuestions3(is, os);
 }
-int assignment2( std::istream& is, std::ostream os){
+int assignment2( std::istream& is, std::ostream& os){
     Image image;
     diagonalQuadPattern(is, os, image);
     drawAsciiImage(is, os, image);
